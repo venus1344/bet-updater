34e1818edd7551619774ddfa2220ca8c5c65f9f4
venus1344 <dmez777@gmail.com>
2024-04-03 23:46:46 +0300

    inital commit

---
+++ src/main/java/org/rdxi/betupdater/BetUpdaterApplication.java
@@ -0,0 +1,13 @@
+package org.rdxi.betupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetUpdaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetUpdaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rdxi/betupdater/BetUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rdxi.betupdater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetUpdaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
